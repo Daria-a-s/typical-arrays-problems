675fee681a6f68b8e08ca7b5ae7f4a4db185e4a4
Daria Sukhova <d.es.tiny.1234545@gmail.com>
2020-09-30 19:15:30 +0300

    feat: fix all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-    return array.length === 0 || array.filter(item => typeof item === "number").length != array.length ? 0 : array.sort((a, b) => a - b)[0];
+    return array && array.length ? array.sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-    return array.length === 0 || array.filter(item => typeof item === "number").length != array.length ? 0 : array.sort((a, b) => a - b)[array.length-1];
+    return array && array.length ? array.sort((a, b) => a - b)[array.length-1] : 0;
 }
 
 exports.avg = function avg (array) {
-    return array.length === 0  ? 0 : array.reduce((sum, curr) => sum + curr)/array.length;
+    return array && array.length ? array.reduce((sum, curr) => sum + curr)/array.length : 0;
 }
